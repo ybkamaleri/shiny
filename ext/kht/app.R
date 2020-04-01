7aca90fa02294c83e5b9eb3e8395184d954bfb3f
Richard White <w@rwhite.no>
2020-04-01 12:44:47 +0200

    Update app.R

--- ext/kht/app.R
+++ ext/kht/app.R
@@ -17,11 +17,11 @@ if (.Platform$OS.type == "windows"){
 
 source("global.R")
 source("covid19.R")
-source("norsyss.R")
-source("norsyss_overview.R")
-source("norsyss_weekly.R")
-source("norsyss_daily.R")
-source("norsyss_information.R")
+#source("norsyss.R")
+#source("norsyss_overview.R")
+#source("norsyss_weekly.R")
+#source("norsyss_daily.R")
+#source("norsyss_information.R")
 
 ui <- tagList(
   useShinyjs(),
@@ -36,9 +36,9 @@ ui <- tagList(
              tabPanel("Covid-19",
                       covid19_ui("covid19", config=config)
              ),
-             tabPanel("NorSySS",
-                      norsyss_ui("norsyss", config=config)
-             ),
+             #tabPanel("NorSySS",
+             #         norsyss_ui("norsyss", config=config)
+             #),
              theme = "fhi.css"
            )
   )
@@ -47,11 +47,11 @@ ui <- tagList(
 server <- function(input, output) {
   callModule(covid19_server, "covid19", config=config)
 
-  callModule(norsyss_server, "norsyss", config=config)
-  callModule(norsyss_overview_server, "norsyss_overview", config=config)
-  callModule(norsyss_weekly_server, "norsyss_weekly", config=config)
-  callModule(norsyss_daily_server, "norsyss_daily", config=config)
-  callModule(norsyss_purpose_server, "norsyss_purpose", config=config)
+  #callModule(norsyss_server, "norsyss", config=config)
+  #callModule(norsyss_overview_server, "norsyss_overview", config=config)
+  #callModule(norsyss_weekly_server, "norsyss_weekly", config=config)
+  #callModule(norsyss_daily_server, "norsyss_daily", config=config)
+  #callModule(norsyss_purpose_server, "norsyss_purpose", config=config)
 }
 
 shinyApp(ui, server)
