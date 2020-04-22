99695342936b9dab6f7b921a170d9d1f8f2b2816
Gry <grmg@fhi.no>
2020-04-22 12:22:13 +0200

    Endret tekst

--- ext/kht/covid19_modelling.R
+++ ext/kht/covid19_modelling.R
@@ -45,10 +45,6 @@ covid19_modelling_ui <- function(id, config) {
         width=12, align="left",
 
         p(
-          strong("Tabell 1"),
-          "viser data for hver 7. dag i det valgte geografiske området",
-          " med et estimert tall og 95% konfidens intervall i parentes.",
-          br(), br(),
 
           " Vi benytter en matematisk modell som simulerer spredningen av",
           "covid-19 i Norge over tid og sted. Modellen tar hensyn til",
@@ -103,7 +99,7 @@ covid19_modelling_ui <- function(id, config) {
       column(
         width=12, align="left",
         br(),
-        p(strong("Figur 1."),"Daglig insidens"),
+        p(strong("Figur 1."),"Daglig insidens med 95% konfidens intervall. Vær oppmerksom på at y-skalaen er forskjellig for de forskjellige geografiske områdene."),
         uiOutput(ns("covid19_ui_modelling_incidence")),
         br(),br(),br()
       )
@@ -115,7 +111,13 @@ covid19_modelling_ui <- function(id, config) {
         #DT::dataTableOutput(ns("covid19_modelling_main"), height = "800px"),
         br(),
         p(
-          strong("Tabell 1."), " beregninger fra FHIs spredningsmodell for covid-19."
+
+          strong("Tabell 1"),
+          "viser beregninger fra FHIs spredningsmodell for covid-19 for hver 7. dag",
+          "i det valgte geografiske området",
+          " med et estimert tall og 95% konfidens intervall i parentes.",
+          br(), br(),
+
         ),
         formattable::formattableOutput(ns("covid19_modelling_main"), height="800px"),
         br(),
